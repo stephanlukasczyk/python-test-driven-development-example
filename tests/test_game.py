f8f93c9a75140c1c04b8a73a099338118b061714
Stephan Lukasczyk <stephan@lukasczyk.me>
2020-04-21 10:29:08 +0200

    Add test for winning with top-bottom diagonal line

--- tests/test_game.py
+++ tests/test_game.py
@@ -55,3 +55,12 @@ def test_when_play_and_whole_vertical_line_then_winner(tictactoe):
     tictactoe.play(2, 2)  # X
     actual = tictactoe.play(1, 3)  # O
     assert actual == "O is the winner"
+
+
+def test_when_play_and_top_bottom_diagonal_line_then_winner(tictactoe):
+    tictactoe.play(1, 1)  # X
+    tictactoe.play(1, 2)  # O
+    tictactoe.play(2, 2)  # X
+    tictactoe.play(1, 3)  # O
+    actual = tictactoe.play(3, 3)  # X
+    assert actual == "X is the winner"
