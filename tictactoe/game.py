38b222b8450b4d82f6cb44a8611280aea88b6796
Stephan Lukasczyk <stephan@lukasczyk.me>
2020-04-21 09:31:56 +0200

    Implement y outside board check

--- tictactoe/game.py
+++ tictactoe/game.py
@@ -3,3 +3,5 @@ class TicTacToe:
     def play(self, x: int, y: int) -> None:
         if x < 1 or x > 3:
             raise RuntimeError("X is outside board")
+        if y < 1 or y > 3:
+            raise RuntimeError("Y is outside board")
