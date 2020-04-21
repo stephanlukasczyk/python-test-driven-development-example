560bcf0b23c625663514ef359e2b0f7d2fbd998f
Stephan Lukasczyk <stephan@lukasczyk.me>
2020-04-21 09:31:26 +0200

    Add test for y outside board check

--- tests/test_game.py
+++ tests/test_game.py
@@ -11,3 +11,8 @@ def tictactoe():
 def test_when_x_outside_board_then_runtime_error(tictactoe):
     with pytest.raises(RuntimeError):
         tictactoe.play(5, 2)
+
+
+def test_when_y_outside_board_then_runtime_error(tictactoe):
+    with pytest.raises(RuntimeError):
+        tictactoe.play(2, 5)
