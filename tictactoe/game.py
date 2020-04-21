d0982f9f74ad2849237934b7a6a6f05a68334a27
Stephan Lukasczyk <stephan@lukasczyk.me>
2020-04-21 09:40:26 +0200

    Implement no winner during game

--- tictactoe/game.py
+++ tictactoe/game.py
@@ -11,11 +11,12 @@ class TicTacToe:
         ]
         self._last_player = "\0"
 
-    def play(self, x: int, y: int) -> None:
+    def play(self, x: int, y: int) -> str:
         self._check_axis(x)
         self._check_axis(y)
         self._set_box(x, y)
         self._last_player = self.next_player
+        return "No winner"
 
     @staticmethod
     def _check_axis(axis: int) -> None:
