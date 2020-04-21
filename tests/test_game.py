992b7855424efbbd1bfc9204046c972f1af756f1
Stephan Lukasczyk <stephan@lukasczyk.me>
2020-04-21 09:29:27 +0200

    Add test for x axis outside board

---
+++ tests/test_game.py
@@ -0,0 +1,13 @@
+import pytest
+
+from tictactoe.game import TicTacToe
+
+
+@pytest.fixture
+def tictactoe():
+    return TicTacToe()
+
+
+def test_when_x_outside_board_then_runtime_error(tictactoe):
+    with pytest.raises(RuntimeError):
+        tictactoe.play(5, 2)
